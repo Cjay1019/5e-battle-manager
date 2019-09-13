b352998d0eb1f16a7b73b9c4e34c47384ba5b6a9
Cjay1019 <cjay741@gmail.com>
2019-09-12 21:43:18 -0400

    added responsive fix for profile dropdown

--- client/src/components/ProfileDropdown.js
+++ client/src/components/ProfileDropdown.js
@@ -9,18 +9,47 @@ import {
 import auth from "../utils/firebase";
 
 class ProfileDropdown extends Component {
+  state = {
+    width: window.innerWidth
+  };
+
+  updateWidth = currWidth => {
+    this.setState({ width: currWidth });
+    console.log(this.state.width);
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", () =>
+      this.updateWidth(window.innerWidth)
+    );
+  }
+
   signOut = () => {
     auth.signOut();
   };
 
   render() {
-    return (
+    return this.state.width < 1350 ? (
+      <MDBDropdown dropleft>
+        <MDBDropdownToggle nav caret>
+          <MDBIcon icon="user" />
+        </MDBDropdownToggle>
+        <MDBDropdownMenu className="dropdown-default">
+          <MDBDropdownItem href="#!"></MDBDropdownItem>
+          <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+          <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+          <MDBDropdownItem onClick={this.signOut} href="#!">
+            Sign Out
+          </MDBDropdownItem>
+        </MDBDropdownMenu>
+      </MDBDropdown>
+    ) : (
       <MDBDropdown>
         <MDBDropdownToggle nav caret>
           <MDBIcon icon="user" />
         </MDBDropdownToggle>
         <MDBDropdownMenu className="dropdown-default">
-          <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+          <MDBDropdownItem href="#!"></MDBDropdownItem>
           <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
           <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
           <MDBDropdownItem onClick={this.signOut} href="#!">
